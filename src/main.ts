9d927ac06cb77296e0bb2a7c69b61be83afe6698
ikabeee <carlglz30@gmail.com>
2025-01-26 04:12:00 -0500

    Change cat tag to projects

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     .setTitle('Project Manager REST API')
     .setDescription('API that manage projects task and teams')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('Projects')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);

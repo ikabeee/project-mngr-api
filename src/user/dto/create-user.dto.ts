0b63c66c8392879f3dfb25c503b6b8264cd6241f
ikabeee <carlglz30@gmail.com>
2025-01-26 15:16:57 -0500

    Attributes for create use dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,17 +1,37 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { role } from 'src/common/enums/role.enum';
+import { statusUser } from 'src/common/enums/statusUser.enum';
 
 export class CreateUserDto {
   @ApiProperty()
   firstName: string;
+
   @ApiProperty()
   lastName: string;
+
   @ApiProperty()
   username: string;
+
   @ApiProperty()
   email: string;
+
   @ApiProperty()
   password: string;
+
   @ApiProperty()
-  @ApiProperty({ enum: ['Active', 'In a team', 'Deleted'] })
+  payrollNumber: string;
+
+  @ApiProperty()
+  hasTeam: boolean;
+
+  @ApiProperty({
+    enum: ['Admin', 'Leader', 'Moderator', 'Collaborator', 'Guest'],
+  })
+  role: role;
+
+  @ApiProperty()
+  teamId: number;
+
+  @ApiProperty({ enum: ['Active', 'Inactive', 'Suspended', 'Deleted'] })
   status: statusUser;
 }

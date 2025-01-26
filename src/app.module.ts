8ecf7103dfdc2872060a86e11ff7376c1cf00f86
ikabeee <carlglz30@gmail.com>
2025-01-26 18:32:16 -0500

    Config env file as global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+  ],
   controllers: [],
   providers: [],
 })

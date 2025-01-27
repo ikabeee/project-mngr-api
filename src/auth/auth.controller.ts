d228c94d33998468796efa8141955bd2fa9a6221
ikabeee <carlglz30@gmail.com>
2025-01-26 23:40:40 -0500

    Finish login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,37 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Post, Body } from '@nestjs/common';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // @Post()
-  // async login (@Body() user: LoginAuthDto) {
-  //   return this.authService.login(user);
-  // }
-
-  // @Post()
-  // create(@Body() createAuthDto: CreateAuthDto) {
-  //   return this.authService.create(createAuthDto);
-  // }
-
-  // @Get()
-  // findAll() {
-  //   return this.authService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.authService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-  //   return this.authService.update(+id, updateAuthDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.authService.remove(+id);
-  // }
+  @HttpCode(HttpStatus.OK)
+  @Post('singIn')
+  async login(@Body() user: LoginAuthDto) {
+    return this.authService.SingIn(user);
+  }
 }

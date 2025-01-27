1681c785c399b8a0b193c9156c59595c6e9e41a1
ikabeee <carlglz30@gmail.com>
2025-01-26 19:29:54 -0500

    Add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     UserModule,
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],

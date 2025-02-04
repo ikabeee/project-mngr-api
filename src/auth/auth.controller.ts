dabf3aea8636d54bd00c05ac54debe75a87dd118
ikabeee <carlglz30@gmail.com>
2025-02-04 01:06:07 -0500

    Fixed eslint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import {
@@ -45,7 +46,10 @@ export class AuthController {
   ) {
     const { userId, securityAnswer } = body;
 
-    const { token } = await this.authService.validateSecurityAnswer(userId, securityAnswer);
+    const { token } = await this.authService.validateSecurityAnswer(
+      userId,
+      securityAnswer,
+    );
 
     response.cookie('jwt', token, {
       httpOnly: true,
